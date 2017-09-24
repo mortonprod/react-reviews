c066c7ddc29d690958e1e6b23d33360f9ece45ca
Alexander Morton <mortonprod@gmail.com>
2017-09-24 01:47:32 +0100

    Add linting to webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,21 +2,31 @@ const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var StyleLintPlugin = require('stylelint-webpack-plugin');
 var webpack = require('webpack');
+
 module.exports = {
-	 entry: {
+	entry: {
 		   index: './src/reviews.tsx'
 	 },
 	 plugins: [
+		   new StyleLintPlugin(),
 		   new CleanWebpackPlugin(['dist']),
 		   new HtmlWebpackPlugin({
 			     title: 'Production',
 					 template: './index.html'
 			 }),
 			 new ExtractTextPlugin("index.css")			 
-	 ],
+	],
+	//tslint:config,
    extensions: ["",".scss", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
    module: {
+		preLoaders: [
+			{
+	   			test: /\.tsx$/,
+	   			loader: 'tslint-loader'
+	   		}
+	   ],
        loaders: [
 		   { test: /\.scss$/, loader: ExtractTextPlugin.extract("css-loader!postcss-loader!sass-loader")},
            { test: /\.css$/, loader: ExtractTextPlugin.extract("css-loader!postcss-loader")},		   
