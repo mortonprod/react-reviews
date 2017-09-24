90de4591ab513510c6b27f800a57b3415a993e94
Alexander Morton <mortonprod@gmail.com>
2017-09-24 01:53:11 +0100

    Add my typings file. ------------------
    TSConfig updated.

---
+++ myTypings.d.ts
@@ -0,0 +1,4 @@
+/**
+ * A review is composed of name,pic,verified,date, star rating and message. 
+ */
+interface review { name: string, pic:string,verified:boolean,date:string, stars: number, message: string }
